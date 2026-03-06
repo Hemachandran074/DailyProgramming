3db78c02954ce6376500b0319ca14949a1647b6a
Hemachandran <hemachandran074@gmail.com>
2026-03-06 20:44:08 +0530

    1784. Check if Binary String Has at Most One Segment of Ones

---
+++ Easy/1784.CheckifBinaryStringHasatMostOneSegmentofOnes.py
@@ -0,0 +1,9 @@
+#Link: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        for i in range(len(s) - 1):
+            if s[i] == "0" and s[i + 1] == "1":
+                return False
+        
+        return True
