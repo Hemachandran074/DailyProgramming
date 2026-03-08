6d0f8b50a8d04ae7072ad57298baf68bc64126c5
Hemachandran <hemachandran074@gmail.com>
2026-03-08 21:20:04 +0530

    1980. Find Unique Binary String

---
+++ Medium/1980.FindUniqueBinaryString.py
@@ -0,0 +1,14 @@
+# Link: https://leetcode.com/problems/find-unique-binary-string/
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        res = ""
+
+        for i in range(len(nums)):
+            if nums[i][i] == "1":
+                res += "0"
+            else:
+                res += "1"
+
+        return res
