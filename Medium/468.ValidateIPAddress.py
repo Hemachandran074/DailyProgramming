c945290b37f43d3d9cc8e7e26096d1963f0bb4a2
Hemachandran <hemachandran074@gmail.com>
2026-03-19 16:06:17 +0530

    468. Validate IP Address

---
+++ Medium/468.ValidateIPAddress.py
@@ -0,0 +1,18 @@
+#Link: https://leetcode.com/problems/validate-ip-address/
+
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        
+        def isIPv4(s):
+            try: return str(int(s)) == s and 0 <= int(s) <= 255
+            except: return False
+        
+        def isIPv6(s):
+            try: return len(s) <= 4 and int(s, 16) >= 0
+            except: return False
+        
+        if queryIP.count(":") == 7 and all(isIPv6(i) for i in queryIP.split(":")):
+            return "IPv6"
+        if queryIP.count(".") == 3 and all(isIPv4(i) for i in queryIP.split(".")):
+            return "IPv4"
+        return "Neither"
