52fc1b59f034b9afe84245d40770da23c60e8888
Hemachandran <hemachandran074@gmail.com>
2026-03-26 21:11:05 +0530

    3548. Equal Sum Grid Partition II

---
+++ Hard/3548.EqualSumGridPartitionII.py
@@ -0,0 +1,43 @@
+#Link: https://leetcode.com/problems/equal-sum-grid-partition-ii/
+
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        def check(g):
+            n = len(g)
+            m = len(grid[0])
+
+            cur = 0
+            seen = {}
+            for i in range(n - 1):
+                for j in range(m):
+                    v = g[i][j]
+                    cur += v
+                    if v in seen:
+                        seen[v][1] = (i, j)
+                    else:
+                        seen[v] = [(i, j), (i, j)]
+                
+                diff = total - cur - cur
+                if diff == 0:
+                    return True
+                if -diff in seen:
+                    fr, fc = seen[-diff][0]
+                    lr, lc = seen[-diff][1]
+                    if m > 1 and i + 1 > 1:
+                        return True
+                    if m > 1 and i + 1 == 1 and (fc == 0 or lc == m - 1):
+                        return True
+                    if m == 1 and (fr == 0 or lr == i):
+                        return True
+        n = len(grid)
+        m = len(grid[0])
+
+        total = sum(grid[i][j] for j in range(m) for i in range(n))
+        if check(grid) or check(grid[::-1]):
+            return True
+        
+        grid = list(zip(*grid))
+
+        if check(grid) or check(grid[::-1]):
+            return True
+        return False
